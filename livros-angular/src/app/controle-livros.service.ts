d4d620e185330850dfe3423ef0f9d438b067e47c
ednaldo1901 <144927754+ednaldo1901@users.noreply.github.com>
2024-08-22 12:13:19 -0300

    comite final

--- livros-angular/src/app/controle-livros.service.ts
+++ livros-angular/src/app/controle-livros.service.ts
@@ -6,9 +6,15 @@ import { Livro } from './livro';
 })
 export class ControleLivrosService {
   private livros: Array<Livro> = [
-    { codigo: 1, codEditora: 1, titulo: 'Use a Cabeça: Java', resumo: 'Uma experiência completa de aprendizado em programação orientada a objetos (OO) e Java.', autores: ['Bert Bates', 'Kathy Sierra'] },
-    { codigo: 2, codEditora: 2, titulo: 'Java, como Programar', resumo: 'Milhões de alunos e profissionais aprenderam programação e desenvolvimento de software com os livros Deitel.', autores: ['Paul Deitel', 'Harvey Deitel'] },
-    { codigo: 2, codEditora: 2, titulo: 'Python, para Programar', resumo: 'Milhões de estudantes e profissionais dominaram a programação e o desenvolvimento de software através dos livros sobre Python. ', autores: ['Edy Ribeiro', 'Cicera Cunha'] },
+    { codigo: 1, codEditora: 1, titulo: 'Use a Cabeça: Java',
+       resumo: 'Uma experiência completa de aprendizado em programação orientada a objetos (OO) e Java.',
+        autores: ['Bert Bates', 'Kathy Sierra'] },
+    { codigo: 2, codEditora: 2, titulo: 'Java, como Programar',
+       resumo: 'Milhões de alunos e profissionais aprenderam programação e desenvolvimento de software com os livros Deitel.',
+        autores: ['Paul Deitel', 'Harvey Deitel'] },
+    { codigo: 2, codEditora: 2, titulo: 'Python, para Programar',
+       resumo: 'Profissionais dominaram a programação e o desenvolvimento de software através dos livros sobre Python. ',
+        autores: ['Edy Ribeiro', 'Cicera Cunha'] },
     // Adicione outros livros aqui
   ];
 
